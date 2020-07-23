76ec7cb5975a4c27d1d230d15210c29c021c69b0
Benoit Delbosc <bdelbosc@nuxeo.com>
2020-07-23 19:16:57 +0200

    wip plotly integration

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import json from '@rollup/plugin-json';
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -46,7 +47,24 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+		// import json file
+		json({
+			// for tree-shaking, properties will be declared as
+			// variables, using either `var` or `const`
+			preferConst: true, // Default: false
+
+			// specify indentation for the generated default export —
+			// defaults to '\t'
+			indent: '  ',
+
+			// ignores indent and generates the smallest code
+			compact: true, // Default: false
+
+			// generate a named export for every property of the JSON object
+			namedExports: true // Default: true
+		})
 	],
 	watch: {
 		clearScreen: false
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,107 @@
+<svelte:head>
+	<script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript" on:load={plotlyLoaded}></script>
+</svelte:head>
+
 <script>
-	export let name;
+
+	let aromas = [
+		{level: 3, trait: 'honey', origin: 'malt'},
+		{level: 2, trait: 'citrus', origin: 'hop'},
+	];
+
+	function add() {
+		aromas = aromas.concat({level: -1, trait: '', origin: 'unknown'});
+	}
+
+	function submit() {
+		console.log(aromas);
+	}
+
+	function clear() {
+		aromas = aromas.filter(t => !t.done);
+	}
+
+	$: remaining = aromas.filter(t => !t.done).length;
+
+
+	import {onMount} from 'svelte';
+	let headerText;
+  export let plotHeader = '';
+	import data from './aroma.json';
+	export let xdata = [data];
+	export let layout = {
+		margin: {l: 0, r: 0, b: 0, t: 0},
+		width: 350,
+		height: 380,
+		sunburstcolorway: ["#FFC300", "#dd408c", "#f1a2d9", "#ff5833", "#259f20", "#97df2d", "#bf6c26", "#df892d", "#ac5812", "#fcd98e", "#5d070e", "#928a80", "#fdf7cb", "#bfac26", "#6b9f20", "#33b5ff", "#33ffe3", "#c7eef3", "#b2aea9"],
+		autocolorscale: true,
+		extendsunburstcolors: true,
+		extendsunburstcolorway: true,
+		font: {size: 22}
+	};
+
+	function plotlyLoaded() {
+		let plotDiv = document.getElementById('plotDiv');
+		let Plot = new Plotly.newPlot(plotDiv, xdata, layout, {showSendToCloud: false});
+	}
+
+	onMount(() => {
+  	headerText = 'On Mount Called !';
+	});
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Aromas</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<div id="plotly">
+  <div>
+    <h1>{plotHeader}</h1>
+  </div>
+  <div id="plotDiv"><!-- Plotly chart will be drawn inside this DIV --></div>
+</div>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+{#each aromas as aroma}
+	<div>
+		<input
+			type=checkbox
+			bind:checked={aroma.done}
+		>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+		<select id="level">
+			<option value="-1">Level?</option>
+			<option value="0">None</option>
+			<option value="1">Low</option>
+			<option value="2">Medium low</option>
+			<option value="3">Medium</option>
+			<option value="3">Medium high</option>
+			<option value="5">High</option>
+		</select>
+
+		<input
+			placeholder="What needs to be done?"
+			bind:value={aroma.trait}
+		>
+		<select id="origin">
+			<option value="unknown">Origin?</option>
+			<option value="malt">Malt</option>
+			<option value="hop">Hop</option>
+			<option value="fermentation">Fermentation</option>
+			<option value="other">Other</option>
+		</select>
+
+	</div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+	Add new
+</button>
+
+<button on:click={clear}>
+	Delete selected
+</button>
+
+<button on:click={submit}>
+	Submit
+</button>
